6a575a6f09659b2005eadc9ffefb8750ac48cce4
kazakovstep <kazakov.step@yandex.ru>
2023-05-26 22:21:04 +0300

    Notes Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.projectlombok:lombok:1.18.22")
 
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
